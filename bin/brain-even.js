ff6f4a36451149ed2e1dd854b4e6f4e761ea5e0f
Tatyana N <mairon.angband@gmail.com>
2024-09-03 03:21:41 +0300

    fix code brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,46 +5,22 @@ import userName from '../src/cli.js';
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-const randomNumbers = _.random(1, 20);
-const answer = readlineSync.question(`Question: ${randomNumbers} \nYour answer: `);
-if (randomNumbers % 2 === 0 && answer === 'yes') {
-  console.log('Correct!');
-} else if (randomNumbers % 2 !== 0 && answer === 'no') {
-  console.log('Correct!');
-} else if (randomNumbers % 2 !== 0 && answer !== 'no') {
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-  process.exit();
-} else if (randomNumbers % 2 === 0 && answer !== 'yes') {
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-  process.exit();
-}
-
-const randomNumbers2 = _.random(1, 20);
-const answer2 = readlineSync.question(`Question: ${randomNumbers2} \nYour answer: `);
-if (randomNumbers2 % 2 === 0 && answer2 === 'yes') {
-  console.log('Correct!');
-} else if (randomNumbers2 % 2 !== 0 && answer2 === 'no') {
-  console.log('Correct!');
-} else if (randomNumbers2 % 2 !== 0 && answer2 !== 'no') {
-  console.log(`'${answer2}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-  process.exit();
-} else if (randomNumbers2 % 2 === 0 && answer2 !== 'yes') {
-  console.log(`'${answer2}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-  process.exit();
-}
-
-const randomNumbers3 = _.random(1, 20);
-const answer3 = readlineSync.question(`Question: ${randomNumbers3} \nYour answer: `);
-if (randomNumbers3 % 2 === 0 && answer3 === 'yes') {
-  console.log('Correct!');
-} else if (randomNumbers3 % 2 !== 0 && answer3 === 'no') {
-  console.log('Correct!');
-} else if (randomNumbers3 % 2 !== 0 && answer3 !== 'no') {
-  console.log(`'${answer3}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-  process.exit();
-} else if (randomNumbers3 % 2 === 0 && answer3 !== 'yes') {
-  console.log(`'${answer3}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-  process.exit();
+let i = 0;
+while (i < 3) {
+  const randomNumbers = _.random(1, 20);
+  const answer = readlineSync.question(`Question: ${randomNumbers} \nYour answer: `);
+  if (randomNumbers % 2 === 0 && answer === 'yes') {
+    console.log('Correct!');
+  } else if (randomNumbers % 2 !== 0 && answer === 'no') {
+    console.log('Correct!');
+  } else if (randomNumbers % 2 !== 0 && answer !== 'no') {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+    process.exit();
+  } else if (randomNumbers % 2 === 0 && answer !== 'yes') {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+    process.exit();
+  }
+  i += 1;
 }
 
 console.log(`Congratulations, ${userName}!`);
